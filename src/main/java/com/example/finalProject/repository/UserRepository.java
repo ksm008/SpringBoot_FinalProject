1be727de9fa7dd2a7055ab75c63a70701a5347f8
ksm008 <yerim63@naver.com>
2024-11-26 09:09:13 +0900

    added KakaoMap api in mainPage

--- src/main/java/com/example/finalProject/repository/UserRepository.java
+++ src/main/java/com/example/finalProject/repository/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // 리스트가 아닌 단일 객체가 필요하기 때문에 List로 받지 않음
     User findByUsername(String username);
 }
