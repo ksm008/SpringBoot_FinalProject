59b8ba5152eb0a87853582e12d4ecda4a89cfae1
ksm008 <yerim63@naver.com>
2024-11-27 10:02:31 +0900

    edited articleNew's kakaomap api

--- src/main/java/com/example/finalProject/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.finalProject.repository;
-
-import com.example.finalProject.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/com/example/finalProject/repository/MediaRepository.java
+++ src/main/java/com/example/finalProject/repository/MediaRepository.java
@@ -1,12 +1,10 @@
 package com.example.finalProject.repository;
 
-import com.example.finalProject.entity.Article;
 import com.example.finalProject.entity.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MediaRepository extends JpaRepository<Media, Long> {
