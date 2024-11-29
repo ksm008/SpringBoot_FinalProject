9d80013682a5aaaeaf3b9e159baa400e0db4fcca
ksm008 <yerim63@naver.com>
2024-11-30 06:38:04 +0900

    minor patches

--- src/main/java/com/example/finalProject/controller/CombinedArticleController.java
+++ src/main/java/com/example/finalProject/controller/CombinedArticleController.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @Controller
 @Slf4j
-public class ArticleController {
+public class CombinedArticleController {
     @Autowired
     CombinedArticleService combinedArticleService;
 
